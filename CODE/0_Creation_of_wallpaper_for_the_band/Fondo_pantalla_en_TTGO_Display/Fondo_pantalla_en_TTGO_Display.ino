6962efa2c4a367adf444664312a0e97795b90a4b
Diegodepab <depablodiegouma@gmail.com>
2024-11-24 22:35:30 +0100

    Optimized wallpaper code to allow more features

--- CODE/0_Creation_of_wallpaper_for_the_band/Fondo_pantalla_en_TTGO_Display/Fondo_pantalla_en_TTGO_Display.ino
+++ CODE/0_Creation_of_wallpaper_for_the_band/Fondo_pantalla_en_TTGO_Display/Fondo_pantalla_en_TTGO_Display.ino
@@ -1,4 +1,4 @@
-#include <TFT_eSPI.h>
+#include <TFT_eSPI.h> // Al usar un TTGO es mejor trabajar con la librería <TFT_eSPI.h> 
 
 // Incluir los archivos .h generados para cada fotograma
 #include "frame_000.h"
@@ -14,26 +14,33 @@
 
 TFT_eSPI tft = TFT_eSPI();  // Crear el objeto TFT
 
-// Crear un array de punteros a los datos de los fotogramas
 const unsigned short* frames[] = {frame_000, frame_001, frame_002, frame_003, frame_004, frame_005, frame_006, frame_007, frame_008, frame_009};
 
+int frameIndex = 0;
+unsigned long previousMillis = 0; // Almacena el tiempo anterior
+const long interval = 100;  // Intervalo entre fotogramas en milisegundos
+
 void setup() {
-  tft.begin();         // Inicia la pantalla correctamente
+  tft.begin(); // Inicia la pantalla correctamente
   tft.setRotation(0);  // Establece la orientación de la pantalla (prueba con 0, 1, 2, o 3)
-  tft.setSwapBytes(true); //IMPORTANTE SIN ESTO LOS COLORES SE INVIERTEN
-  tft.fillScreen(TFT_WHITE);
+  tft.setSwapBytes(true);  // Esto es necesario o los colores no sé leeran correctamente
+  tft.fillScreen(TFT_WHITE); // establecer que aquellas partes que no sé cubran sea en blanco inicialmente
 }
 
 void loop() {
+  unsigned long currentMillis = millis();  // Captura el tiempo actual
 
+  // Si el tiempo actual menos el tiempo anterior es mayor que el intervalo
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;  // Actualiza el tiempo anterior
 
+    // Dibuja el fotograma 
+    tft.pushImage(0, 0, 160, 235, frames[frameIndex]);
 
-  // FONDO DE PANTALLA
-  for (int i = 0; i < 10; i++) {
-    // Dibuja cada fotograma en la pantalla
-    tft.pushImage(0, 0, 160, 235, frames[i]);
-    
-    // Ajusta el tiempo de espera entre fotogramas (velocidad de la animación)
-    delay(100);  // 100 ms entre fotogramas, ajusta este valor para cambiar la velocidad
+    // Avanza al siguiente fotograma
+    frameIndex++;
+    if (frameIndex >= 10) {
+      frameIndex = 0;  // Vuelve al primer fotograma cuando termine
+    }
   }
 }
